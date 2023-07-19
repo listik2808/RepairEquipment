8d71e312a272528dd4b09bf527f1b868a14c5740
Евгений <listopadov_94@mail.ru>
2023-07-19 15:00:38 +0300

    управления и слежения камерой

--- Assets/Scripts/Infrastructure/Game.cs
+++ Assets/Scripts/Infrastructure/Game.cs
@@ -1,9 +1,23 @@
-﻿using UnityEngine;
+﻿using Scripts.Services.Input;
+using UnityEngine;
 
 namespace Scripts.Infrastructure
 {
     public class Game : MonoBehaviour
     {
+        public static IInputService InputService;
 
+        public Game()
+        {
+            RegisterInputService();
+        }
+
+        private static void RegisterInputService()
+        {
+            if (Application.isEditor)
+                InputService = new StandaloneInputService();
+            else
+                InputService = new MobileInputService();
+        }
     }
 }
