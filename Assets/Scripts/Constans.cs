db4b502cef5569c7aea246b1e10e2a0b49a16d7f
Евгений <listopadov_94@mail.ru>
2023-07-19 15:02:42 +0300

    слежения камерой

---
+++ Assets/Scripts/Constans.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Scripts
+{
+    public static class Constans
+    {
+        public const float Epsilon = 0.001f;
+    }
+}
---
+++ Assets/Scripts/Hero/HeroMove.cs
@@ -0,0 +1,47 @@
+using Scripts.CameraLogic;
+using Scripts.Infrastructure;
+using Scripts.Services.Input;
+using UnityEngine;
+
+namespace Scripts.Hero
+{
+[RequireComponent(typeof(CharacterController))]
+    public class HeroMove : MonoBehaviour
+    {
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField]private float _movmentSpeed;
+
+        private IInputService _inputService;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _inputService = Game.InputService;
+        }
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            CameraFollow();
+        }
+
+        private void Update()
+        {
+            Vector3 movementVector = Vector3.zero;
+            if(_inputService.Axis.sqrMagnitude > Constans.Epsilon)
+            {
+                movementVector = _camera.transform.TransformDirection(_inputService.Axis);
+                movementVector.y = 0;
+                movementVector.Normalize();
+                transform.forward = movementVector;
+            }
+            movementVector += Physics.gravity;
+            _characterController.Move(_movmentSpeed * movementVector * Time.deltaTime);
+        }
+
+        private void CameraFollow()
+        {
+            _camera.GetComponent<CameraFollow>().Follow(gameObject);
+        }
+    }
+}
